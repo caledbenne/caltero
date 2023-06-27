f9c761fc3a540d0f3eef98d6879a8efddb61a2fb
caledbenne <caledbenne@gmail.com>
2023-06-26 23:13:58 -0500

    added about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
-import { Hero, Navbar } from './components';
+import { AboutComponent, Hero, Navbar } from './components';
 
 function App() {
 
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/caltero/">
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar/>
           <Hero/>
         </div>
-        {/* <About/>
-        <Experience/>
+        <AboutComponent/>
+        {/* <Experience/>
         <Tech/>
         <Works/>
         <Feedbacks/>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import { Hero } from "./Hero";
 import { Navbar } from "./Navbar";
+import { About, AboutComponent } from "./About";
 
-export { Hero, Navbar };
+export { Hero, Navbar, About, AboutComponent };
