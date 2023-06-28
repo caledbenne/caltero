49c67b08e950ab250683e4e19a4f598cdb1b8b53
caledbenne <caledbenne@gmail.com>
2023-06-28 00:00:23 -0500

    minor formatting updates

--- src/components/cards/ContactCard.tsx
+++ src/components/cards/ContactCard.tsx
@@ -10,7 +10,7 @@ interface ContactCardProps {
 export function ContactCard({contact, index}: ContactCardProps) {
     return (<motion.div
     variants={fadeIn('right', 'spring', 0.5*index, 0.75)}
-    className="flex-1 px-10"
+    className="flex-1 px-10 py-2"
 >
         <a href={contact.url} className="bg-tertiary rounded-[40px] py-2 px-12  flex justify-evenly items-center hover:bg-secondary max-w-md">
         <img src={contact.icon} alt={contact.name} className="w-16 h-16 object-contain pr-5"/>
